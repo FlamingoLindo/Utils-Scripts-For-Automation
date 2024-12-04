e15f892c33ee824976cbb271e4a1294b11a2e554
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2024-12-04 10:13:15 -0300

    feat: apple

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install dependencies
+pip install -r requirements.txt
+
+# Clone repository into the current directory
+git clone https://github.com/FlamingoLindo/Utils-Scripts-For-Automation
